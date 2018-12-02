3f7af787456373ad9008bfce1c28a8b4994ad1a1
Zishan Ansari <kinnngg786@gmail.com>
2018-12-03 03:17:56 +0530

    Added a sample unit test

--- my_test.rb
+++ my_test.rb
@@ -1,11 +1,12 @@
 require 'test/unit'
+require_relative 'main'
 
 class MyTest < Test::Unit::TestCase
 
   # Called before every test method runs. Can be used
   # to set up fixture information.
   def setup
-    # Do nothing
+
   end
 
   # Called after every test method runs. Can be used to tear
@@ -19,5 +20,11 @@ class MyTest < Test::Unit::TestCase
   def test_fail
 
     fail('Not implemented')
+  end
+
+  def test_wordsearch
+    wordlist = File.readlines('words.txt').map { |line| line.strip }
+    words = %w[donations beverages placement performed metallica thousands]
+    assert_equal ["a", "at", "do", "nation", "on", "age", "be", "era", "ever", "me", "place", "for", "form", "or", "per", "perform", "all", "metal", "tall", "and", "sand", "thousand", "us", "art", "to", "in", "as", "pet", "rid", "arm"], searchwords(words, wordlist)
   end
 end
